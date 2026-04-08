8c69ba5dc132cbb8b6eb0ff8998c382e7ad5ab48
domody <domody11@gmail.com>
2026-04-08 17:17:23 +0100

    feat: add create short link dialog with useForm hook

--- resources/js/components/app-sidebar-header.tsx
+++ resources/js/components/app-sidebar-header.tsx
@@ -3,12 +3,36 @@ import { SidebarTrigger } from '@/components/ui/sidebar';
 import type { BreadcrumbItem as BreadcrumbItemType } from '@/types';
 import { Button } from './ui/button';
 import { PlusIcon } from 'lucide-react';
+import { useState } from 'react';
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from './ui/dialog';
+import { Input } from './ui/input';
+import { Field, FieldError, FieldGroup } from './ui/field';
+import { Label } from './ui/label';
+import {
+    InputGroup,
+    InputGroupAddon,
+    InputGroupInput,
+    InputGroupText,
+} from './ui/input-group';
+import { useForm } from '@inertiajs/react';
+import { Spinner } from './ui/spinner';
 
 export function AppSidebarHeader({
     breadcrumbs = [],
 }: {
     breadcrumbs?: BreadcrumbItemType[];
 }) {
+    const [open, setOpen] = useState(false);
+
     return (
         <header className="flex h-16 shrink-0 items-center gap-2 border-b border-sidebar-border/50 px-6 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 md:px-4">
             <div className="flex items-center gap-2">
@@ -16,11 +40,97 @@ export function AppSidebarHeader({
                 <Breadcrumbs breadcrumbs={breadcrumbs} />
             </div>
             <div className="ml-auto flex items-center gap-2">
-                <Button>
-                    <PlusIcon data-icon="inline-start" />
-                    Create Short Link
-                </Button>
+                <Dialog open={open} onOpenChange={setOpen}>
+                    <DialogTrigger asChild>
+                        <Button>
+                            <PlusIcon data-icon="inline-start" />
+                            Create Short Link
+                        </Button>
+                    </DialogTrigger>
+                    <CreateLinkDialogContent onSuccess={() => setOpen(false)} />
+                </Dialog>
             </div>
         </header>
+    );
+}
+
+type LinkForm = {
+    original_url: string;
+    code: string;
+};
+
+function CreateLinkDialogContent({ onSuccess }: { onSuccess: () => void }) {
+    const { data, setData, post, errors, processing } = useForm<LinkForm>({
+        original_url: '',
+        code: '',
+    });
+
+    const submit = (e: React.SubmitEvent) => {
+        e.preventDefault();
+        post('/create', { onSuccess });
+    };
+
+    return (
+        <DialogContent className="top-[15%] translate-y-0">
+            <form onSubmit={submit} className='grid gap-4'>
+                <DialogHeader>
+                    <DialogTitle>Create short link</DialogTitle>
+                    <DialogDescription>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit.
+                    </DialogDescription>
+                </DialogHeader>
+                <FieldGroup>
+                    <Field>
+                        <Label htmlFor="original-link">Original URL</Label>
+                        <Input
+                            id="original-link"
+                            name="original-link"
+                            placeholder="example.link.com"
+                            value={data.original_url}
+                            onChange={(e) =>
+                                setData('original_url', e.target.value)
+                            }
+                            required
+                        />
+                        {errors.original_url && (
+                            <FieldError>{errors.original_url}</FieldError>
+                        )}
+                    </Field>
+                    <Field>
+                        <Label htmlFor="custom-code">
+                            Custom Code (optional)
+                        </Label>
+                        <InputGroup>
+                            <InputGroupAddon>
+                                <InputGroupText>
+                                    url-shorter.test/
+                                </InputGroupText>
+                            </InputGroupAddon>
+                            <InputGroupInput
+                                id="custom-code"
+                                name="custom-code"
+                                placeholder="custom"
+                                className="pl-0.5!"
+                                value={data.code}
+                                onChange={(e) =>
+                                    setData('code', e.target.value)
+                                }
+                            />
+                        </InputGroup>
+                        {errors.code && <FieldError>{errors.code}</FieldError>}
+                    </Field>
+                </FieldGroup>
+                <DialogFooter>
+                    <DialogClose asChild>
+                        <Button variant="outline">Cancel</Button>
+                    </DialogClose>
+                    <Button type="submit" disabled={processing}>
+                        {processing && <Spinner />}
+                        Create
+                    </Button>
+                </DialogFooter>
+            </form>
+        </DialogContent>
     );
 }
