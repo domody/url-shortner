c3a682115b2538dab702d38d18008a945585e238
domody <domody11@gmail.com>
2026-04-19 00:35:42 +0100

    fix: update tests to route to correct route

--- tests/Feature/Auth/VerificationNotificationTest.php
+++ tests/Feature/Auth/VerificationNotificationTest.php
@@ -16,7 +16,7 @@ test('sends verification notification', function () {
 
     $this->actingAs($user)
         ->post(route('verification.send'))
-        ->assertRedirect(route('home'));
+        ->assertRedirect(route('landing'));
 
     Notification::assertSentTo($user, VerifyEmail::class);
 });
